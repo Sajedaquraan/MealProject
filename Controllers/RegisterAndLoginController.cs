659901de9673daf92cb50a7c709f0d41272b77b3
sajedaquraan <sajedaalquraan1@gmail.com>
2024-09-03 22:57:06 +0300

    update pages

---
+++ Controllers/RegisterAndLoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MealProject.Controllers
+{
+    public class RegisterAndLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
